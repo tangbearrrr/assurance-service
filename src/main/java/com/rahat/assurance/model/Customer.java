0c1fb50e28049cf4dfabf22b6e69866d7452e347
tangbearrrr <rh.sarawasee@gmail.com>
2024-06-18 22:14:36 +0700

    Add jsonproperty

--- src/main/java/com/rahat/assurance/model/Customer.java
+++ src/main/java/com/rahat/assurance/model/Customer.java
@@ -1,6 +1,7 @@
 package com.rahat.assurance.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -11,11 +12,23 @@ public class Customer {
 
     @Id
     private String id;
+
+    @JsonProperty(value = "first_name")
     private String firstName;
+
+    @JsonProperty(value = "last_name")
     private String lastName;
+
+    @JsonProperty(value = "address")
     private String address;
+
+    @JsonProperty(value = "mobile_no")
     private String mobileNo;
+
+    @JsonProperty(value = "image_url")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String imageUrl;
+
+    @JsonProperty(value = "status")
     private String status;
 }
