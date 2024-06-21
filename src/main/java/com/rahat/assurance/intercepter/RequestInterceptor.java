c03596dd14864f65e9db85579e34e136176d647c
tangbearrrr <rh.sarawasee@gmail.com>
2024-06-21 16:52:01 +0700

    Add interceptor

---
+++ src/main/java/com/rahat/assurance/intercepter/RequestInterceptor.java
@@ -0,0 +1,31 @@
+package com.rahat.assurance.intercepter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Log4j2
+public class RequestInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        long startTime = (Long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long executeTime = endTime - startTime;
+
+        log.info("API response time=[{} ms] status=[{}] method[{}] service=[{}]",
+                executeTime,
+                response.getStatus(),
+                request.getMethod(),
+                request.getRequestURI());
+    }
+}
